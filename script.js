669c81cafc3492fa0e2118c66c3e168c98c54e36
AZMAIN13 <azmainnabi0063@gmail.com>
2024-12-11 12:35:55 -0500

    Update and rename script.js.txt to script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Example of JavaScript to handle interactivity
+document.querySelector('.shop-now-btn').addEventListener('click', function() {
+    alert('You are now redirected to the book shop!');
+});
